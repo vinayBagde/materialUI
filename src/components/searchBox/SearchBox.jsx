7a40a2f11b56da426449e88833598157b526ab1e
vinayBagde <vinaybagde0@gmail.com>
2024-11-10 21:03:06 +0530

    used weather API

--- src/components/searchBox/SearchBox.jsx
+++ src/components/searchBox/SearchBox.jsx
@@ -1,24 +1,52 @@
 import "./SearchBox.css";
-import {useState} from "react";
+import { useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 
 export default function SearchBox() {
+  let [city, setCity] = useState("");
+  const API_url = "https://api.openweathermap.org/data/2.5/weather";
 
-    let [city, setCity] = useState("");
-    let handleChange = (event) => {
-        setCity(event.target.value);
-    }
+  const API_Key = "df83a9107b7946fc310020cc6f9f6e84";
 
-    let handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(city);
+  let getWeatherInfo = async (city) => {
+    let response = await fetch(`${API_url}?q=${city}&appid=${API_Key}&units=metric`);
+    let jsonResponse = await response.json();
+    let result = {
+        temp: jsonResponse.main.temp,
+        temp_min: jsonResponse.main.temp_min,
+        temp_max: jsonResponse.main.temp_max,
+        humidity: jsonResponse.main.humidity,
+        feels_like: jsonResponse.main.feels_like,
+        weather: jsonResponse.weather[0].description
     }
+    console.log(jsonResponse);
+    console.log(result);
+  };
+
+  let handleChange = (event) => {
+    setCity(event.target.value);
+  };
+
+  let handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(city);
+    setCity("");
+    getWeatherInfo(city);
+  };
   return (
-    <div className="searchBox" onSubmit={handleSubmit}>
+    <div className="searchBox">
       <h3>search for the weather</h3>
-      <form action="./action">
-        <TextField id="city" label="City Name" variant="outlined" required value={city} onChange={handleChange}/>
+      <form action="./action" onSubmit={handleSubmit}>
+        <TextField
+          id="city"
+          label="City Name"
+          variant="outlined"
+          required
+          value={city}
+          onChange={handleChange}
+        />
         <br />
         <br />
         <Button variant="contained" type="submit">
